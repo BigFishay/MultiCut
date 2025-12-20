b256270a10efe3f2a3cc0f6a6c9725cc7ce56c20
BigFishay <gamebrycew@gmail.com>
2025-12-19 21:53:06 -0800

    Created classes and skeleton for project management. Cut: main builder, EX: executer, Program: actual program object

---
+++ MultiCut/Program/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+
+        string name;
+        string location;
+        string description;
+
+        public Program(string name, string location, string description)
+        {
+            this.name = name;
+            this.location = location;
+            this.description = description;
+        }
+
+        public Program(string json)
+        {
+            var parts = json.Trim('{', '}').Split(',');
+            foreach (var part in parts)
+            {
+                string[] kv = part.Split(':');
+                string key = kv[0].Trim('\"');
+                string value = kv[1].Trim('\"');
+                switch (key)
+                {
+                    case "name":
+                        name = value;
+                        break;
+                    case "location":
+                        location = value;
+                        break;
+                    case "description":
+                        description = value;
+                        break;
+                }
+            }
+            if (name == null || location == null || description == null)
+            {
+                throw new ArgumentException("Invalid JSON format");
+            }
+        }
+
+        public string toJson()
+        {
+            return $"{{\"name\":\"{name}\",\"location\":\"{location}\",\"description\":\"{description}\"}}";
+        }
+
+        public override string ToString()
+        {
+            return location;
+        }
+
+    }
+}
---
+++ MultiEX/MultiEX/Execute.cs
@@ -0,0 +1,13 @@
+﻿using Program;
+
+namespace MultiEX
+{
+    internal class Execute
+    {
+        static void Main(string[] args)
+        {
+            string jsonLoc = args[0];
+
+        }
+    }
+}
